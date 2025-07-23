5b7630ba303a32668575b9a8a1c99ddb079a4344
Elsizy <Elsizymanuel@gmail.com>
2025-07-23 13:11:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBj-MK1oOk6lIJZT8KrSsllwSqoMHfUkzQ",
-  authDomain: "anns8-5fc26.firebaseapp.com",
-  projectId: "anns8-5fc26",
-  storageBucket: "anns8-5fc26.appspot.com",
-  messagingSenderId: "259361189676",
-  appId: "1:259361189676:web:a9d54cee391b9f0f468689"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-document.getElementById("signupForm").addEventListener("submit", async function(event) {
+document.getElementById("signupForm").addEventListener("submit", async function (event) {
   event.preventDefault();
 
-  const email = document.getElementById("email").value.trim(); // ← agora usa o input de email
+  const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirmPassword").value;
   const referral = document.getElementById("referral").value.trim();
