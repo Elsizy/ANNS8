74079aac6805498e931bb4ea0d301369f5f9b94d
Elsizy <Elsizymanuel@gmail.com>
2025-07-23 19:57:42 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -7,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBj-MK1oOk6lIJZT8KrSsllwSqoMHfUkzQ",
   authDomain: "anns8-5fc26.firebaseapp.com",
   projectId: "anns8-5fc26",
-  storageBucket: "anns8-5fc26.firebasestorage.app",
+  storageBucket: "anns8-5fc26.appspot.com", // corrigido aqui o domínio
   messagingSenderId: "259361189676",
   appId: "1:259361189676:web:a9d54cee391b9f0f468689"
 };
