b4ec704737f512d8a6d90d998dd77becbf11b249
Elsizy <Elsizymanuel@gmail.com>
2025-07-24 10:28:22 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,11 @@
+// products.js
+export const PRODUTOS = [
+  { nome: "Nex 1", preco: 5000, comissao: 750 },
+  { nome: "Nex 2", preco: 15000, comissao: 2250 },
+  { nome: "Nex 3", preco: 40000, comissao: 6000 },
+  { nome: "Nex 4", preco: 90000, comissao: 13500 },
+  { nome: "Nex 5", preco: 200000, comissao: 30000 },
+  { nome: "Nex 6", preco: 600000, comissao: 90000 },
+  { nome: "Nex 7", preco: 1400000, comissao: 210000 },
+  { nome: "Nex 8", preco: 3200000, comissao: 480000 },
+];
