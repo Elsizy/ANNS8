6eccd561deb94d2c8f4c929f2f96af81b8cac828
Elsizy <Elsizymanuel@gmail.com>
2025-07-24 10:23:23 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,20 @@
 // firebase-config.js
-var firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence, // <- mantém a sessão após fechar o navegador
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  get,
+  set,
+  update,
+  child,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyBj-MK1oOk6lIJZT8KrSsllwSqoMHfUkzQ",
   authDomain: "anns8-5fc26.firebaseapp.com",
   databaseURL: "https://anns8-5fc26-default-rtdb.firebaseio.com",
@@ -9,5 +24,18 @@ var firebaseConfig = {
   appId: "1:259361189676:web:a9d54cee391b9f0f468689"
 };
 
-// Inicializa o Firebase v8
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence); // persiste a sessão
+const db = getDatabase(app);
+
+export {
+  app,
+  auth,
+  db,
+  ref,
+  get,
+  set,
+  update,
+  child,
+};
