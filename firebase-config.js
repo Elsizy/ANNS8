61f0c44bc4d143cc9eaf7eefb96ab13b30703fcd
Elsizy <Elsizymanuel@gmail.com>
2025-07-24 07:40:02 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,20 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js"; // ADICIONADO
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBj-MK1oOk6lIJZT8KrSsllwSqoMHfUkzQ",
   authDomain: "anns8-5fc26.firebaseapp.com",
-  databaseURL: "https://anns8-5fc26-default-rtdb.firebaseio.com", // ADICIONADO
+  databaseURL: "https://anns8-5fc26-default-rtdb.firebaseio.com",
   projectId: "anns8-5fc26",
   storageBucket: "anns8-5fc26.appspot.com",
   messagingSenderId: "259361189676",
   appId: "1:259361189676:web:a9d54cee391b9f0f468689"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getDatabase(app); // ADICIONADO
+const db = getDatabase(app);
 
 export { auth, db };
