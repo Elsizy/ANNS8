be317d70bb6a91e19e28bc847fad262a899b5714
Elsizy <Elsizymanuel@gmail.com>
2025-08-14 15:17:32 +0100

    Update retirada.js

--- retirada.js
+++ retirada.js
@@ -71,7 +71,7 @@ onAuthStateChanged(auth, async (user) => {
     // 3) Carrega contas bancárias (se não tiver, manda criar)
     const accSnap = await get(ref(db, `usuarios/${uid}/bankAccounts`));
     if (!accSnap.exists()) {
-      alert("Você precisa cadastrar uma conta bancária antes de retirar.");
+      showFeedback(("error", "Você precisa cadastrar uma conta bancária antes de retirar.");
       window.location.href = "conta.html";
       return;
     }
