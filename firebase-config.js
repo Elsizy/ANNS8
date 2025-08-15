5cf2f02a243a9901da8714f0ed49224439d3cc5e
Elsizy <Elsizymanuel@gmail.com>
2025-08-15 17:47:34 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,20 @@
-// Importa funções principais do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+// firebase-config.js (modular)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-// Configuração do seu projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBj-MK1oOk6lIJZT8KrSsllwSqoMHfUkzQ",
   authDomain: "anns8-5fc26.firebaseapp.com",
   databaseURL: "https://anns8-5fc26-default-rtdb.firebaseio.com",
   projectId: "anns8-5fc26",
-  storageBucket: "anns8-5fc26.firebasestorage.app",
+  storageBucket: "anns8-5fc26.appspot.com",
   messagingSenderId: "259361189676",
   appId: "1:259361189676:web:a9d54cee391b9f0f468689"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, db };
