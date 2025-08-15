7ac1f5a9e5357c3002c2bd2f62ec1a9662e71173
Elsizy <Elsizymanuel@gmail.com>
2025-08-15 20:08:36 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
-// firebase-config.js (CDN, modular)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+// firebase-config.js (CDN, modular, v10.12.2)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDnd53WofoxaKUfTtQiyA6B3KkCYicUFVE",
   authDomain: "aesinc-1b8a9.firebaseapp.com",
   databaseURL: "https://aesinc-1b8a9-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "aesinc-1b8a9",
-  storageBucket: "aesinc-1b8a9.appspot.com", // ← bucket correto
+  storageBucket: "aesinc-1b8a9.appspot.com", // nome de bucket correto
   messagingSenderId: "241657630880",
   appId: "1:241657630880:web:3342e177b87480427fb9c5"
 };
