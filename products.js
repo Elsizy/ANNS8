73f5e36b0dc55231d8f9e14f8216a54c5046afc7
Elsizy <Elsizymanuel@gmail.com>
2025-09-24 09:56:31 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,13 +1,13 @@
 // products.js
 export const PRODUTOS = [
-  { id: "nex-1",  nome: "Projeto 1",  preco: 5000,    comissao: 500  },
-  { id: "nex-2",  nome: "Projeto 2",  preco: 12000,   comissao: 1200 },
-  { id: "nex-3",  nome: "Projeto 3",  preco: 32000,   comissao: 3200 },
-  { id: "nex-4",  nome: "Projeto 4",  preco: 53000,   comissao: 5300},
-  { id: "nex-5",  nome: "Projeto 5",  preco: 112000,  comissao: 12000},
-  { id: "nex-6",  nome: "Projeto 6",  preco: 300000,  comissao: 30000},
-  { id: "nex-7",  nome: "Projeto 7",  preco: 750000, comissao: 75000},
-  { id: "nex-8",  nome: "Projeto 8",  preco: 1650000, comissao: 165000}
+  { id: "nex-1",  nome: "Projeto 1",  preco: 5000,    comissao: 600  },
+  { id: "nex-2",  nome: "Projeto 2",  preco: 12000,   comissao: 1440 },
+  { id: "nex-3",  nome: "Projeto 3",  preco: 32000,   comissao: 3440 },
+  { id: "nex-4",  nome: "Projeto 4",  preco: 53000,   comissao: 6360},
+  { id: "nex-5",  nome: "Projeto 5",  preco: 112000,  comissao: 14400},
+  { id: "nex-6",  nome: "Projeto 6",  preco: 300000,  comissao: 36000},
+  { id: "nex-7",  nome: "Projeto 7",  preco: 750000, comissao: 90000},
+  { id: "nex-8",  nome: "Projeto 8",  preco: 1650000, comissao: 198000}
 ];
 
 // limites
