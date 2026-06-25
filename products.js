c7abf1203f4816df0a13c1030dbb3f6157d85a68
Elsizy <Elsizymanuel@gmail.com>
2026-06-25 15:44:32 +0100

    Update products.js

--- products.js
+++ products.js
@@ -4,49 +4,49 @@ export const PRODUTOS = [
    nome: "TANK 1",  
    preco: 5000,    
    comissao: 600,
-   imagem:"images/bot1.png"
+   imagem:"bot1.png"
   },
   { id: "nex-2",  
    nome: "TANK 2",  
    preco: 12000,   
    comissao: 1440, 
-   imagem:"images/bot2.png"
+   imagem:"bot2.png"
   },
   { id: "nex-3",  
    nome: "TANK 3",  
    preco: 32000,   
    comissao: 3440, 
-   imagem:"images/bot3.png"
+   imagem:"bot3.png"
   },
   { id: "nex-4",  
    nome: "TANK 4",  
    preco: 53000,   
    comissao: 6360, 
-   imagem:"images/bot4.png"
+   imagem:"bot4.png"
   },
   { id: "nex-5",  
    nome: "TANK 5",  
    preco: 112000,  
    comissao: 14400, 
-   imagem:"images/bot5.png"
+   imagem:"bot5.png"
   },
   { id: "nex-6",  
    nome: "TANK 6",  
    preco: 300000,  
    comissao: 36000, 
-   imagem:"images/bot6.png"
+   imagem:"bot6.png"
   },
   { id: "nex-7",  
    nome: "TANK 7", 
    preco: 750000, 
    comissao: 90000, 
-   imagem:"images/bot7.png"
+   imagem:"bot7.png"
   },
   { id: "nex-8",  
    nome: "TANK 8",  
    preco: 1650000, 
    comissao: 198000,
-   imagem:"images/bot8.png"
+   imagem:"bot8.png"
   }
 ];
 
