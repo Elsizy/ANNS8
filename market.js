5de8fe3e2b81050e2939c876c9bdb174e6bae25b
Elsizy <Elsizymanuel@gmail.com>
2026-06-30 11:59:50 +0100

    Update market.js

--- market.js
+++ market.js
@@ -57,8 +57,8 @@ async function loadMarket() {
 
                 const symbol =
                     item.symbol.replace(
-                        "USDT",
-                        "/USDT"
+"USDT",
+'<span class="usdt">/USDT</span>'
                     );
 
                 const price =
