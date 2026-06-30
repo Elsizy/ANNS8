b79a11384cbe9f4bc0822ed456eed9a9a526ff86
Elsizy <Elsizymanuel@gmail.com>
2026-06-30 11:09:19 +0100

    Update market.js

--- market.js
+++ market.js
@@ -93,7 +93,7 @@ async function loadMarket() {
                                 alt="${symbol}"
                             >
 
-                            <span>
+                            <span class="market-symbol">
                                 ${symbol}
                             </span>
 
