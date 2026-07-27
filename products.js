a370e960315324b18aa5b491d8bb43c302db43ce
Elsizy <Elsizymanuel@gmail.com>
2026-07-27 19:44:30 +0100

    Refactor comissao to use getter for dynamic calculation
    Replaced static 'comissao' property with a getter that calculates commission based on 'preco' and 'percentage'.

--- products.js
+++ products.js
@@ -3,65 +3,82 @@ export const PRODUTOS = [
   { id: "nex-1",  
    nome: "TANK 1",  
    preco: 5000,    
-   comissao: 600,
    duracao: 90,
    percentage: 0.08,
+   get comissao() {
+    return this.preco * this.percentage;
+   }, 
    imagem:"bot1.png" 
   },
+  
   { id: "nex-2",  
    nome: "TANK 2",  
    preco: 12000,   
-   comissao: 1440,
    duracao: 90,
    percentage: 0.08,
+   get comissao() {
+    return this.preco * this.percentage;
+   }, 
    imagem:"bot2.png"
   },
   { id: "nex-3",  
    nome: "TANK 3",  
-   preco: 32000,   
-   comissao: 3440, 
+   preco: 32000,    
    duracao: 60,
    percentage: 0.10,
+   get comissao() {
+   return this.preco * this.percentage;
+   }, 
    imagem:"bot3.png"
   },
   { id: "nex-4",  
    nome: "TANK 4",  
    preco: 53000,   
-   comissao: 6360,
    duracao: 90,
    percentage: 0.08,
+   get comissao() {
+   return this.preco * this.percentage;
+  }, 
    imagem:"bot4.png"
   },
   { id: "nex-5",  
    nome: "TANK 5",  
    preco: 112000,  
-   comissao: 14400,
    duracao: 90,
    percentage: 0.08,
+   get comissao() {
+   return this.preco * this.percentage;
+      }, 
    imagem:"bot5.png"
   },
   { id: "nex-6",  
    nome: "TANK 6",  
    preco: 300000,  
-   comissao: 36000,
    duracao: 90,
    percentage: 0.08,
+   get comissao() {
+   return this.preco * this.percentage;
+    }, 
    imagem:"bot6.png"
   },
   { id: "nex-7",  
    nome: "TANK 7", 
-   preco: 750000, 
-   comissao: 90000, 
+   preco: 750000,  
    duracao: 90,
    percentage: 0.08,
+   get comissao() {
+   return this.preco * this.percentage;
+  }, 
    imagem:"bot7.png"
   },
   { id: "nex-8",  
    nome: "TANK 8",  
    preco: 1650000, 
-   comissao: 198000,
    duracao: 90, 
    percentage: 0.08,
+   get comissao() {
+   return this.preco * this.percentage;
+    }, 
    imagem:"bot8.png"
   }
 ];
